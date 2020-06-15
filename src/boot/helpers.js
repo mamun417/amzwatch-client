d6f942822c9d1e5ab94ef3bf2f537ea5d2cd579b
Younus <younusdev@gmail.com>
2020-06-15 13:43:01 +0600

    fix time issue

--- src/boot/helpers.js
+++ src/boot/helpers.js
@@ -4,16 +4,16 @@ import lodash from 'lodash'
 export default function ({store, app, Vue}) {
     Vue.prototype.$moment = moment;
     Vue.prototype.$_      = lodash;
-    
+
     Vue.prototype.$singleLoaderTrue = function (name) {
         store.dispatch('ui/setSingleLoaderToTrue', name);
     }
-    
+
     Vue.prototype.$singleLoaderFalse = function (name) {
         store.dispatch('ui/setSingleLoaderToFalse', name);
     }
-    
+
     Vue.prototype.$timestampToDate = function (timestamp, format = "DD MM YYYY, h:mm:ss a") {
-        return moment(parseInt(timestamp) / 1000).format(format)
+        return moment(parseInt(timestamp)).format(format)
     }
 }
