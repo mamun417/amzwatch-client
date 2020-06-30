2b11348a1bd2c5847d84f302d4e214682820c199
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-06-30 18:07:19 +0600

    improve DomainUptimeDetails page

--- src/components/domain-uptime/DomainUptimeDetails.vue
+++ src/components/domain-uptime/DomainUptimeDetails.vue
@@ -123,54 +123,7 @@
 
         <q-separator class="q-mt-lg" />
 
-        <div class="q-mt-md relative-position">
-            <div class="text-h6">Latest Logs</div>
-
-            <q-list v-if="uptimeLogs.length" class="q-py-md" dense>
-                <q-item class="text-primary text-subtitle2 text-bold">
-                    <q-item-section>Event</q-item-section>
-                    <q-item-section class="text-center">Reason</q-item-section>
-                    <q-item-section class="text-right">Last checked</q-item-section>
-                </q-item>
-
-                <q-item
-                    v-for="(log, index) in uptimeLogs"
-                    class="q-mb-sm shadow-1 text-weight-medium"
-                    :key="index"
-                    clickable
-                >
-                    <q-item-section>
-                        <q-badge
-                            :color="log.status !== 'ok' ? 'red' : 'green'"
-                            style="max-width: 70px"
-                        >
-                            <q-icon name="arrow_downward" color="white" class="q-mr-xs text-bold" />
-                            <span class="text-bold">{{log.status !== 'ok' ? 'Down': 'Up'}}</span>
-                        </q-badge>
-                    </q-item-section>
-
-                    <q-item-section class="text-center">
-                        <span
-                            :class="[log.status !== 'ok' ? 'text-red' : 'text-green']"
-                        >{{log.status !== 'ok' ? 'Connection Timeout' : 'OK (200)'}}</span>
-                    </q-item-section>
-
-                    <q-item-section class="text-right">{{$timestampToDate(log.logged_at)}}</q-item-section>
-                </q-item>
-            </q-list>
-
-            <div v-else class="text-center q-py-md text-weight-bold">No logs found</div>
-
-            <div v-if="uptimeLogsPaginationMeta.last_page > 1" class="q-py-sm flex flex-center">
-                <pagination
-                    :current_page="uptimeLogsPaginationMeta.current_page"
-                    :last_page="uptimeLogsPaginationMeta.last_page"
-                    @handlePagination="uptimeLogsPaginationHandle"
-                />
-            </div>
-
-            <q-inner-loading color="primary" :showing="!!singleLoader.domainUptimeLogsLoader" />
-        </div>
+        <latest-logs/>
     </div>
 
     <!-- <div v-else class="text-subtitle2 text-center q-pa-lg">No Data found</div> -->
@@ -182,9 +135,11 @@ import { mapGetters } from "vuex";
 import Pagination from "components/pagination/Pagination";
 import { now } from "moment";
 import ApexChart from 'vue-apexcharts'
+import LatestLogs from "components/domain-uptime/LatestLogs";
 
 export default {
     components: {
+        LatestLogs,
         QCChart,
         Pagination,
         ApexChart
@@ -272,7 +227,6 @@ export default {
 
     mounted() {
         this.getDomainUptimePingTimeline();
-        this.getDomainUptimeLogs();
         this.getDomainUptimeLatestDowntime();
 
         // this.interval = setInterval(() => {
@@ -286,7 +240,6 @@ export default {
 
     computed: {
         ...mapGetters({
-            uptimeLogsPaginationMeta: "domain_uptime/paginationMeta",
             singleLoader: "ui/singleLoaderStatus"
         })
     },
@@ -309,24 +262,10 @@ export default {
                     interval: "min"
                 })
                 .then(res => {
-                    this.uptimePingTimeline =
-                        res.data.domainUptimePingTimeline.data;
+                    this.uptimePingTimeline = res.data.domainUptimePingTimeline.data;
 
                     this.generatePingDataFromTimeline(this.uptimePingTimeline);
-
-                    // push chart data
-                    this.uptimePingTimeline.forEach((timeline, index) => {
-                        let currentTimelineDay = this.$moment(timeline.logged_at).format("DD hh:mm")
-
-                        this.series[0].data.push(
-                            {
-                                x: currentTimelineDay,
-                                y: timeline.info.avg
-                            }
-                        );
-                    });
-
-                    this.chartDataReady = true;
+                    this.pushChartDate();
                 })
                 .catch(err => {
                     //handle error
@@ -431,31 +370,19 @@ export default {
                 });
         },
 
-        getDomainUptimeLogs() {
-            this.$singleLoaderTrue("domainUptimeLogsLoader");
+        pushChartDate() {
+            this.uptimePingTimeline.forEach((timeline, index) => {
+                let currentTimelineDay = this.$moment(timeline.logged_at).format("DD hh:mm")
 
-            this.$store
-                .dispatch("domain_uptime/getDomainUptimeLogs", {
-                    vm: this,
-                    project_id: this.$route.params.project_id,
-                    log_type: "ping"
-                })
-                .then(res => {
-                    this.uptimeLogs = res.data.domainUptimeLogs.data;
-                    this.$singleLoaderFalse("domainUptimeLogsLoader");
-                })
-                .catch(err => {
-                    //handle error
-                    this.$singleLoaderFalse("domainUptimeLogsLoader");
-                });
-        },
+                this.series[0].data.push(
+                    {
+                        x: currentTimelineDay,
+                        y: timeline.info.avg
+                    }
+                );
+            });
 
-        uptimeLogsPaginationHandle(page) {
-            this.$store
-                .dispatch("domain_uptime/updateUptimeLogsCurrentPage", page)
-                .then(() => {
-                    this.getDomainUptimeLogs();
-                });
+            this.chartDataReady = true;
         }
     }
 };
