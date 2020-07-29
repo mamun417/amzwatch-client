c9a07d2f259e4c13097dd227d1aa5d3d51c1ea29
Younus <younusdev@gmail.com>
2020-07-29 17:10:02 +0600

    redesign guest post update modal

--- src/components/modals/GuestPostsCheckServiceActivateDeactivateModal.vue
+++ src/components/modals/GuestPostsCheckServiceActivateDeactivateModal.vue
@@ -1,61 +1,40 @@
 <template>
-    <q-dialog
-        :value="show"
-        @input="$emit('update:show', $event)"
-    >
+    <q-dialog :value="show" @input="$emit('update:show', $event)">
         <q-card style="min-width: 400px">
             <q-card-section class="bg-primary text-white">
                 <div class="text-h6">Update Guest Link Checker Service</div>
             </q-card-section>
 
             <q-card-section class="text-center q-pt-xl">
-                <div class="q-mb-lg">
-                    <div class="text-caption q-mb-sm">Please fill guest domain url first</div>
-                    <q-input
-                        v-model="guestDomain"
-                        label="Guest domain url"
-                        class="q-mx-md" standout="bg-primary text-white"
-                        hint="without path. ex. https://exonhost.com"
-                        error-message="invalid domain url"
-                        :error="guestDomainInvalid"
-                        @focus="guestDomainInputFocused = true"
-                        @blur="guestDomainInputFocused = false"
-                        dense
-                    />
-                </div>
-
-                <q-list v-show="guestDomain && !guestDomainInvalid && !guestDomainInputFocused">
-                    <q-item
-                        v-if=""
-                        v-for="(gl, key) in guestLinksForUpdate"
-                        :key="key" clickable
-                    >
+                <q-list>
+                    <q-item v-for="(gl, key) in guestLinksForUpdate" :key="key" clickable>
                         <div class="items-center row">
-                            <q-btn
-                                @click="$delete(guestLinksForUpdate, key)"
-                                :label="key + 1"
-                                color="primary"
-                                class="q-mr-sm"
-                                flat
-                            />
+                            <q-btn :label="key + 1" class="q-mr-sm" flat />
                         </div>
 
                         <div class="row">
                             <q-input
                                 v-model="gl.guestUrl"
                                 label="Guest URL"
-                                class="col-12 q-mb-xs" standout="bg-primary text-white"
-                                error-message="path must start with / or keep blank"
-                                :prefix="guestDomain" :error="gl.guestUrlError"
-                                stack-label dense hide-bottom-space
+                                class="col-12 q-mb-xs"
+                                standout="bg-primary text-white"
+                                error-message="Input valid guest url"
+                                :error="gl.guestUrlError"
+                                stack-label
+                                dense
+                                hide-bottom-space
                             />
                             <q-input
                                 v-model="gl.hostedUrl"
-                                label="Hosted URL"
-                                class="col-12" standout="bg-primary text-white"
+                                label="My URL"
+                                class="col-12"
+                                standout="bg-primary text-white"
                                 error-message="path must start with / or keep blank"
-                                :prefix="projectInfo.domain.url" :error="gl.hostedUrlError"
-                                stack-label dense hide-bottom-space
+                                :prefix="projectInfo.domain.url"
+                                :error="gl.hostedUrlError"
+                                stack-label
+                                dense
+                                hide-bottom-space
                             />
                         </div>
 
@@ -63,191 +42,235 @@
                             <q-btn
                                 v-if="guestLinksForUpdate.length > 1"
                                 @click="$delete(guestLinksForUpdate, key)"
-                                icon="clear" color="warning"
-                                class="q-ml-sm" size="sm"
+                                icon="clear"
+                                color="warning"
+                                class="q-ml-sm"
+                                size="sm"
                                 dense
                             />
                         </div>
                     </q-item>
 
-                    <div class="text-xs text-grey-8 q-mt-sm">Note: Keep blank if you want to use home url & for path
+                    <div class="text-xs text-grey-8 q-mt-sm">
+                        Note: Keep blank if you want to use home url & for path
                         start with /
                     </div>
 
                     <q-btn
                         @click="pushToGuestLinksForUpdate"
-                        color="primary" label="Add another url"
-                        no-caps size="sm" class="q-mt-md"
+                        color="primary"
+                        label="Add another url"
+                        no-caps
+                        size="sm"
+                        class="q-mt-md"
                     />
                 </q-list>
 
                 <div class="text-left q-mt-lg">
                     <q-toggle
                         v-model="status"
-                        true-value="active" false-value="inactive"
-                        checked-icon="done" unchecked-icon="clear"
+                        true-value="active"
+                        false-value="inactive"
+                        checked-icon="done"
+                        unchecked-icon="clear"
                         :label="status"
-                        color=""
+                        color
                     />
                 </div>
             </q-card-section>
 
             <q-card-actions align="right" class="text-primary">
                 <q-btn
-                    label="Update" class="q-mr-md"
-                    :disable="guestDomainInvalid || !guestDomain"
+                    label="Update"
+                    class="q-mr-md"
                     @click="serviceActivateDeactivateHandle"
                     flat
                 />
             </q-card-actions>
 
-            <q-inner-loading color="primary" :showing="!!singleLoader.guestLinkCheckerUpdateLoader"/>
-
+            <q-inner-loading
+                color="primary"
+                :showing="!!singleLoader.guestLinkCheckerUpdateLoader"
+            />
         </q-card>
     </q-dialog>
 </template>
 
 <script>
-    import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 
-    export default {
-        props   : {
-            show       : {
-                type   : Boolean,
-                default: false
-            },
-            projectInfo: {
-                type   : Object,
-                default: () => ({})
-            }
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            default: false,
         },
-        data() {
-            return {
-                guestDomain            : '',
-                guestDomainInputFocused: true,
-                guestLinksForUpdate    : [
-                    {
-                        guestUrl      : '',
-                        guestUrlError : false,
-                        hostedUrl     : '',
-                        hostedUrlError: false
-                    }
-                ],
-                status                 : 'active'
-            }
+        projectInfo: {
+            type: Object,
+            default: () => ({}),
         },
-        computed: {
-            guestDomainInvalid() {
-                return !this.guestDomainInputFocused
-                    && (!this.guestDomain || !this.guestDomain.includes('://'))
-            },
-            ...mapGetters({
-                singleLoader : 'ui/singleLoaderStatus'
-            })
+    },
+    data() {
+        return {
+            guestLinksForUpdate: [
+                {
+                    guestUrl: "",
+                    guestUrlError: false,
+                    hostedUrl: "",
+                    hostedUrlError: false,
+                },
+            ],
+            status: "active",
+        };
+    },
+    computed: {
+        ...mapGetters({
+            singleLoader: "ui/singleLoaderStatus",
+        }),
+
+        updateDisabled() {
+            let status = false;
+
+            this.guestLinksForUpdate.forEach((v) => {
+                if (
+                    !v.guestUrl ||
+                    (v.guestUrl && !v.guestUrlError) ||
+                    (v.hostedUrl && v.hostedUrl[0] !== "/")
+                )
+                    status = true;
+            });
+
+            return status;
         },
-        methods : {
-            pushToGuestLinksForUpdate() {
-                this.guestLinksForUpdate.push({
-                    guestUrl      : '',
-                    guestUrlError : false,
-                    hostedUrl     : '',
-                    hostedUrlError: false
-                })
-            },
+    },
+    methods: {
+        pushToGuestLinksForUpdate() {
+            this.guestLinksForUpdate.push({
+                guestUrl: "",
+                guestUrlError: false,
+                hostedUrl: "",
+                hostedUrlError: false,
+            });
+        },
 
-            serviceActivateDeactivateHandle() {
+        serviceActivateDeactivateHandle() {
+            this.$singleLoaderTrue("guestLinkCheckerUpdateLoader");
 
-                this.$singleLoaderTrue('guestLinkCheckerUpdateLoader');
+            let guestLinks = [];
 
-                let guestLinks = []
-
-                this.guestLinksForUpdate.map((obj, key) => {
-                    guestLinks.push({
-                        guest_post_url: `${this.guestDomain}${obj.guestUrl}`,
-                        holding_url   : `${this.projectInfo.domain.url}${obj.hostedUrl}`
-                    })
-                })
-                this.$store.dispatch('guest_links/updateGuestPostsCheckService', {
-                    vm        : this,
+            this.guestLinksForUpdate.map((obj, key) => {
+                guestLinks.push({
+                    guest_post_url: obj.guestUrl,
+                    holding_url: `${this.projectInfo.domain.url}${obj.hostedUrl}`,
+                });
+            });
+            this.$store
+                .dispatch("guest_links/updateGuestPostsCheckService", {
+                    vm: this,
                     project_id: this.projectInfo.id,
-                    inputs    : {
-                        guest_domain: this.guestDomain,
-                        urls        : guestLinks,
-                        status      : this.status
-                    }
+                    inputs: {
+                        urls: guestLinks,
+                        status: this.status,
+                    },
                 })
-                    .then(res => {
-                        this.$singleLoaderFalse('guestLinkCheckerUpdateLoader');
+                .then((res) => {
+                    this.$singleLoaderFalse("guestLinkCheckerUpdateLoader");
 
-                        this.$q.notify({
-                            color   : 'positive',
-                            message : 'Guest link checker service has been updated Successful',
-                            position: 'top'
-                        });
-
-                        this.$emit('serviceUpdated', res.data.project);
-                        this.$emit('update:show', false);
-                    })
-                    .catch(err => {
-                        this.$singleLoaderFalse('guestLinkCheckerUpdateLoader');
-                        //handle error
+                    this.$q.notify({
+                        color: "positive",
+                        message:
+                            "Guest link checker service has been updated Successful",
+                        position: "top",
                     });
-            }
+
+                    this.$emit("serviceUpdated", res.data.project);
+                    this.$emit("update:show", false);
+                })
+                .catch((err) => {
+                    this.$singleLoaderFalse("guestLinkCheckerUpdateLoader");
+                    //handle error
+                });
         },
-        watch   : {
-            projectInfo        : {
-                handler() {
-                    if (this.projectInfo.id) {
-                        if (
-                            this.projectInfo.hasOwnProperty('domain_use_for')
-                            && this.projectInfo.domain_use_for.hasOwnProperty('guest_posts_check_service')
-                        ) {
-                            let status = this.projectInfo.domain_use_for.guest_posts_check_service.status;
+    },
+    watch: {
+        projectInfo: {
+            handler() {
+                if (this.projectInfo.id) {
+                    this.$singleLoaderTrue("guestLinkCheckerUpdateLoader");
 
-                            this.status = status || 'inactive'
-                        }
+                    if (
+                        this.projectInfo.hasOwnProperty("domain_use_for") &&
+                        this.projectInfo.domain_use_for.hasOwnProperty(
+                            "guest_posts_check_service"
+                        )
+                    ) {
+                        let status = this.projectInfo.domain_use_for
+                            .guest_posts_check_service.status;
 
-                        this.$store.dispatch('guest_links/getAllGuestPosts', {
-                            vm        : this,
-                            project_id: this.projectInfo.id
-                        })
-                            .then(res => {
-                                let guestPosts = res.data.linksInGuestPosts.data;
-
-                                if (guestPosts.length) {
-                                    this.guestLinksForUpdate     = [];
-                                    this.guestDomainInputFocused = false;
-                                }
-
-                                guestPosts.map((obj, key) => {
-                                    let parsedGuestUrl  = new URL(obj.guest_post_url)
-                                    let parsedHostedUrl = new URL(obj.holding_url)
-
-                                    this.guestDomain = parsedGuestUrl.origin
-
-                                    this.guestLinksForUpdate.push({
-                                        guestUrl : parsedGuestUrl.pathname,
-                                        hostedUrl: parsedHostedUrl.pathname
-                                    })
-                                })
-                            })
-                            .catch(err => {
-                                //handle error
-                            })
+                        this.status = status || "inactive";
                     }
-                },
-                deep     : true,
-                immediate: true
+
+                    this.$store
+                        .dispatch("guest_links/getAllGuestPosts", {
+                            vm: this,
+                            project_id: this.projectInfo.id,
+                        })
+                        .then((res) => {
+                            let guestPosts = res.data.linksInGuestPosts.data;
+
+                            if (guestPosts.length) {
+                                this.guestLinksForUpdate = [];
+                            }
+
+                            guestPosts.map((obj, key) => {
+                                let parsedHostedUrl = new URL(obj.holding_url);
+
+                                this.guestLinksForUpdate.push({
+                                    guestUrl: obj.guest_post_url,
+                                    hostedUrl: parsedHostedUrl.pathname,
+                                });
+                            });
+
+                            this.$singleLoaderFalse(
+                                "guestLinkCheckerUpdateLoader"
+                            );
+                        })
+                        .catch((err) => {
+                            this.$singleLoaderFalse(
+                                "guestLinkCheckerUpdateLoader"
+                            );
+                        });
+                }
             },
-            guestLinksForUpdate: {
-                handler() {
-                    this.guestLinksForUpdate.map((obj, key) => {
-                        obj.guestUrlError  = !!obj.guestUrl && obj.guestUrl[0] !== '/'
-                        obj.hostedUrlError = !!obj.hostedUrl && obj.hostedUrl[0] !== '/'
-                    })
-                },
-                deep: true
-            }
-        }
-    }
+            deep: true,
+            immediate: true,
+        },
+        guestLinksForUpdate: {
+            handler() {
+                this.guestLinksForUpdate.map((obj, key) => {
+                    if (obj.guestUrl) {
+                        try {
+                            let parsedGuestdUrl = new URL(obj.guestUrl);
+
+                            // obj.guestUrlError = !!!parsedGuestdUrl.origin;
+                            obj.guestUrlError =
+                                !!!parsedGuestdUrl.origin ||
+                                (this.projectInfo.domain &&
+                                    parsedGuestdUrl.origin ===
+                                        this.projectInfo.domain.url);
+                        } catch {
+                            obj.guestUrlError = true;
+                        }
+                    } else {
+                        obj.guestUrlError = true;
+                    }
+
+                    obj.hostedUrlError =
+                        !!obj.hostedUrl && obj.hostedUrl[0] !== "/";
+                });
+            },
+            deep: true,
+        },
+    },
+};
 </script>
