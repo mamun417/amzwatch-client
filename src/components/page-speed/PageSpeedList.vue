ab0c2a8a8a6539c4eecb19e7ee8e1ab41bee0679
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-07-30 17:50:28 +0600

    page speed full result show done

--- src/components/page-speed/PageSpeedList.vue
+++ src/components/page-speed/PageSpeedList.vue
@@ -208,65 +208,6 @@
             </q-expansion-item>
         </q-list>
 
-        <!--<q-dialog v-model="activeFullResultModal">
-            <q-card>
-                <q-card-section>
-                    <div
-                        class="text-center text-primary text-subtitle1 text-weight-bold q-mb-sm"
-                    >Full Results</div>
-
-                    <div class="q-gutter-lg q-mb-md q-mt-md">
-                        <span v-for="n in ['positive', 'warning', 'negative']" >
-                            <q-btn dense round :color="n" size="xs"/>
-                            <span class="vertical-middle"> {{ $_.upperFirst(n) }}</span>
-                        </span>
-                    </div>
-
-                    <q-list>
-                        <div
-                            v-for="(key, index) in fullSpeedAuditsName"
-                            v-if="fullSpeedResultPage.meta.lhr_desktop_result.audits[key].displayValue"
-                            class="row full-width q-pa-sm justify-between items-center shadow-1 q-mb-sm"
-                            :class="$q.screen.lt.sm ? 'text-center' : ''"
-                            :key="index"
-                        >
-                            &lt;!&ndash;<pre>{{fullSpeedResultPage.meta['lhr_desktop_result'].audits[key]}}</pre>&ndash;&gt;
-                            <div class="col-12 col-sm-4 q-mb-sm">
-                                {{ $_.startCase($_.replace(key, new RegExp("-","g"), ' ')) }}
-                            </div>
-
-                            <div class="col-12 col-sm">
-                                <span
-                                    v-for="(device, index) in ['lhr_desktop_result', 'lhr_mobile_result']"
-                                    class="q-mx-md"
-                                >
-                                    <q-icon
-                                        :name="device === 'lhr_desktop_result' ? 'desktop_mac' : 'phone_android'"
-                                        size="20px"
-                                        color="primary"
-                                    />
-                                    <q-circular-progress
-                                        v-if="fullSpeedResultPage.meta.hasOwnProperty(device) && fullSpeedResultPage.meta[device]"
-                                        class="q-mx-xs text-bold"
-                                        :class="[`text-${calculateScoreType(fullSpeedResultPage.meta[device].audits[key].score * 100).color}`]"
-                                        :value="fullSpeedResultPage.meta[device].audits[key].score * 100"
-                                        size="60px"
-                                        font-size="10px"
-                                        :color="calculateScoreType(fullSpeedResultPage.meta[device].audits[key].score * 100).color"
-                                        track-color="grey-4"
-                                        show-value
-                                    >
-                                        {{ fullSpeedResultPage.meta[device].audits[key].displayValue }}
-                                    </q-circular-progress>
-                                    <span v-else class="q-ml-xs text-warning">Pending</span>
-                                </span>
-                            </div>
-                        </div>
-                    </q-list>
-                </q-card-section>
-            </q-card>
-        </q-dialog>-->
-
         <full-page-speed-result
             :active.sync="activeFullResultModal"
             :full-speed-result-page="fullSpeedResultPage"
