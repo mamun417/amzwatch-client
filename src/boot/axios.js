a124ab71b2a9cfc6c15f18c3ff120277d3a6dd0e
Mohammed Younus <younusdev@gmail.com>
2020-08-16 13:47:21 +0600

    fix Unauthorized handle

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,12 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
-export default function ({ store, Vue, router }) {
+export default function({ store, Vue, router }) {
     axios.defaults.withCredentials = true;
-    Vue.prototype.$http = axios
+    Vue.prototype.$http = axios;
 
-    let apiBase = process.env.PROD ? process.env.PROD_API_ENDPOINT : process.env.API_ENDPOINT;
+    let apiBase = process.env.PROD
+        ? process.env.PROD_API_ENDPOINT
+        : process.env.API_ENDPOINT;
 
-    let temToken = ''
+    let temToken = "";
     let isRefreshing = false;
 
     Vue.prototype.$apiBase = apiBase;
@@ -14,115 +16,135 @@ export default function ({ store, Vue, router }) {
     // handle before req happen
     axios.interceptors.request.use(
         req => {
-            const token = localStorage.getItem('token');
+            const token = localStorage.getItem("token");
 
             if (token) {
-                req.headers['Authorization'] = 'Bearer ' + token
+                req.headers["Authorization"] = "Bearer " + token;
             }
 
-            return req
+            return req;
         },
         err => {
-            Promise.reject(err).then(r => r)
+            Promise.reject(err).then(r => r);
         }
-    )
+    );
 
     // handle before res is send to client
     axios.interceptors.response.use(
         res => {
-            return res
+            return res;
         },
         err => {
-            if (err.response.status === 498 && err.response.data.message === 'Token Expired/Invalid') {
-
+            if (
+                err.response.status === 498 &&
+                err.response.data.message === "Token Expired/Invalid"
+            ) {
                 if (!isRefreshing) {
                     isRefreshing = true;
 
-                    return axios.get(apiBase + '/refresh')
-                        .then(async (res) => {
+                    return axios
+                        .get(apiBase + "/refresh")
+                        .then(async res => {
                             temToken = res.data.bearerToken;
 
-                            store.dispatch('auth/updateToken', temToken)
+                            store.dispatch("auth/updateToken", temToken);
 
-                            axios.defaults.headers.common['Authorization'] = 'Bearer ' + temToken
+                            axios.defaults.headers.common["Authorization"] =
+                                "Bearer " + temToken;
 
                             isRefreshing = false;
 
-                            return axios(err.config)
+                            return axios(err.config);
                         })
                         .catch(err => {
                             // think what we need to do. i think do logout cz we cant refresh so do manual login
                             isRefreshing = false;
-                        })
+                        });
                 } else {
                     return new Promise((resolve, reject) => {
                         let intrvl = setInterval(async () => {
                             if (!isRefreshing) {
                                 clearInterval(intrvl);
 
-                                axios.defaults.headers.common['Authorization'] = 'Bearer ' + temToken
+                                axios.defaults.headers.common["Authorization"] =
+                                    "Bearer " + temToken;
 
-                                resolve(axios(err.config))
+                                resolve(axios(err.config));
                             }
                         }, 300);
-                    })
+                    });
                 }
-            }
-            else if (err.response.status === 422 && ['Bearer token invalid', 'Refresh token not found'].includes(err.response.data.message)) {
-                store.dispatch('auth/logout').then(() => {
-                    router.push('/auth/login')
+            } else if (
+                err.response.status === 422 &&
+                ["Bearer token invalid", "Refresh token not found"].includes(
+                    err.response.data.message
+                )
+            ) {
+                store.dispatch("auth/logout").then(() => {
+                    router.push("/auth/login");
 
-                    return Promise.reject(err)
-                })
-            }
-            else if (err.response.status === 403 && err.response.data.message === 'Forbidden') {
-                return router.push('/')
-            }
-            else {
-                return Promise.reject(err)
+                    return Promise.reject(err);
+                });
+            } else if (
+                err.response.status === 401 &&
+                err.response.data.message === "Unauthorized"
+            ) {
+                store.dispatch("auth/logout").then(() => {
+                    router.push("/auth/login");
+
+                    return Promise.reject(err);
+                });
+            } else if (
+                err.response.status === 403 &&
+                err.response.data.message === "Forbidden"
+            ) {
+                return router.push("/");
+            } else {
+                return Promise.reject(err);
             }
         }
-    )
+    );
 
-    Vue.prototype.$api = function (apiPath) {
+    Vue.prototype.$api = function(apiPath) {
         return this.$apiBase + apiPath;
-    }
+    };
 
-    Vue.prototype.$get = function (path, queryParams) {
+    Vue.prototype.$get = function(path, queryParams) {
         return new Promise((resolve, reject) => {
-            this.$http.get(this.$api(path), { params: queryParams })
+            this.$http
+                .get(this.$api(path), { params: queryParams })
                 .then(res => {
-                    resolve(res)
+                    resolve(res);
                 })
                 .catch(err => {
-                    reject(err)
-                })
-        })
-    }
+                    reject(err);
+                });
+        });
+    };
 
-    Vue.prototype.$post = function (path, payload) {
+    Vue.prototype.$post = function(path, payload) {
         return new Promise((resolve, reject) => {
-            this.$http.post(this.$api(path), payload)
+            this.$http
+                .post(this.$api(path), payload)
                 .then(res => {
-                    resolve(res)
+                    resolve(res);
                 })
                 .catch(err => {
-                    reject(err)
-                })
-        })
-    }
+                    reject(err);
+                });
+        });
+    };
 
-    Vue.prototype.$patch = function (path, payload) {
+    Vue.prototype.$patch = function(path, payload) {
         return new Promise((resolve, reject) => {
-            this.$http.patch(this.$api(path), payload)
+            this.$http
+                .patch(this.$api(path), payload)
                 .then(res => {
-                    resolve(res)
+                    resolve(res);
                 })
                 .catch(err => {
-                    reject(err)
-                })
-        })
-    }
-
-
+                    reject(err);
+                });
+        });
+    };
 }
--- src/components/modals/FullPageSpeedResult.vue
+++ src/components/modals/FullPageSpeedResult.vue
@@ -2,72 +2,166 @@
     <q-dialog :value="active" @hide="$emit('update:active', false)">
         <q-card>
             <q-card-section>
-                <div class="text-center text-primary text-h6 text-weight-bold q-mb-sm">Full Results</div>
+                <div
+                    class="text-center text-primary text-h6 text-weight-bold q-mb-sm"
+                >
+                    Full Results
+                </div>
 
                 <div class="q-gutter-lg q-my-lg text-center">
                     <span
-                        v-for="(n, index) in [{c: 'positive', n: 'Good'}, {c:'warning', n: 'Average'}, {c:'negative', n: 'poor'}]"
+                        v-for="(n, index) in [
+                            { c: 'positive', n: 'Good' },
+                            { c: 'warning', n: 'Average' },
+                            { c: 'negative', n: 'poor' }
+                        ]"
                         :key="index"
                     >
-                        <q-btn dense round :color="n.c" class="q-mr-sm" size="xs" />
+                        <q-btn
+                            dense
+                            round
+                            :color="n.c"
+                            class="q-mr-sm"
+                            size="xs"
+                        />
                         <span class="vertical-middle">{{ n.n }}</span>
                     </span>
                 </div>
 
                 <!-- tried to do it simply but got duplicate code blocks. solve it by render function -->
-                <q-list v-for="(key, index) in fullSpeedAuditsNames" class :key="index">
+                <q-list
+                    v-for="(key, index) in fullSpeedAuditsNames"
+                    class
+                    :key="index"
+                >
                     <q-item
-                        v-if="fullSpeedResultPage.meta.lhr_desktop_result.audits[key].displayValue"
+                        v-if="
+                            fullSpeedResultPage.meta.lhr_desktop_result.audits[
+                                key
+                            ].displayValue
+                        "
                         class="shadow-1 q-my-xs"
                         clickable
                     >
                         <q-item-section class="text-weight-bold" no-wrap>
                             <template v-if="$q.screen.lt.sm">
-                                <q-item-label
-                                    class="text-center q-my-sm"
-                                >{{ $_.startCase($_.replace(key, new RegExp("-","g"), ' ')) }}</q-item-label>
+                                <q-item-label class="text-center q-my-sm">{{
+                                    $_.startCase(
+                                        $_.replace(
+                                            key,
+                                            new RegExp("-", "g"),
+                                            " "
+                                        )
+                                    )
+                                }}</q-item-label>
                                 <q-item-label>
                                     <div
-                                        v-for="(device, index) in ['lhr_desktop_result', 'lhr_mobile_result']"
+                                        v-for="(device, index) in [
+                                            'lhr_desktop_result',
+                                            'lhr_mobile_result'
+                                        ]"
                                         class="text-weight-bold q-my-sm flex items-center justify-between"
                                         :key="index"
                                     >
                                         <q-icon
                                             size="30px"
-                                            :name="device === 'lhr_desktop_result' ? 'desktop_mac' : 'phone_android'"
-                                            :color="$parent.calculateScoreType(fullSpeedResultPage.meta[device].audits[key].score * 100).color"
+                                            :name="
+                                                device === 'lhr_desktop_result'
+                                                    ? 'desktop_mac'
+                                                    : 'phone_android'
+                                            "
+                                            :color="
+                                                $parent.calculateScoreType(
+                                                    fullSpeedResultPage.meta[
+                                                        device
+                                                    ].audits[key].score * 100
+                                                ).color
+                                            "
                                         />
                                         <div
-                                            v-if="fullSpeedResultPage.meta.hasOwnProperty(device) && fullSpeedResultPage.meta[device]"
-                                            :class="[`text-${$parent.calculateScoreType(fullSpeedResultPage.meta[device].audits[key].score * 100).color}`]"
-                                        >{{ fullSpeedResultPage.meta[device].audits[key].displayValue }}</div>
+                                            v-if="
+                                                fullSpeedResultPage.meta.hasOwnProperty(
+                                                    device
+                                                ) &&
+                                                    fullSpeedResultPage.meta[
+                                                        device
+                                                    ]
+                                            "
+                                            :class="[
+                                                `text-${
+                                                    $parent.calculateScoreType(
+                                                        fullSpeedResultPage
+                                                            .meta[device]
+                                                            .audits[key].score *
+                                                            100
+                                                    ).color
+                                                }`
+                                            ]"
+                                        >
+                                            {{
+                                                fullSpeedResultPage.meta[device]
+                                                    .audits[key].displayValue
+                                            }}
+                                        </div>
 
-                                        <div v-else class="text-warning">Pending</div>
+                                        <div v-else class="text-warning">
+                                            Pending
+                                        </div>
                                     </div>
                                 </q-item-label>
                             </template>
-                            <template
-                                v-else
-                            >{{ $_.startCase($_.replace(key, new RegExp("-","g"), ' ')) }}</template>
+                            <template v-else>{{
+                                $_.startCase(
+                                    $_.replace(key, new RegExp("-", "g"), " ")
+                                )
+                            }}</template>
                         </q-item-section>
 
                         <q-item-section v-if="!$q.screen.lt.sm" no-wrap side>
                             <div
-                                v-for="(device, index) in ['lhr_desktop_result', 'lhr_mobile_result']"
+                                v-for="(device, index) in [
+                                    'lhr_desktop_result',
+                                    'lhr_mobile_result'
+                                ]"
                                 class="text-weight-bold q-my-xs"
                                 :key="index"
                             >
                                 <span
-                                    v-if="fullSpeedResultPage.meta.hasOwnProperty(device) && fullSpeedResultPage.meta[device]"
-                                    :class="[`text-${$parent.calculateScoreType(fullSpeedResultPage.meta[device].audits[key].score * 100).color}`]"
-                                >{{ fullSpeedResultPage.meta[device].audits[key].displayValue }}</span>
+                                    v-if="
+                                        fullSpeedResultPage.meta.hasOwnProperty(
+                                            device
+                                        ) && fullSpeedResultPage.meta[device]
+                                    "
+                                    :class="[
+                                        `text-${
+                                            $parent.calculateScoreType(
+                                                fullSpeedResultPage.meta[device]
+                                                    .audits[key].score * 100
+                                            ).color
+                                        }`
+                                    ]"
+                                    >{{
+                                        fullSpeedResultPage.meta[device].audits[
+                                            key
+                                        ].displayValue
+                                    }}</span
+                                >
                                 <span v-else class="text-warning">Pending</span>
 
                                 <q-icon
                                     class="q-ml-md"
                                     size="30px"
-                                    :name="device === 'lhr_desktop_result' ? 'desktop_mac' : 'phone_android'"
-                                    :color="$parent.calculateScoreType(fullSpeedResultPage.meta[device].audits[key].score * 100).color"
+                                    :name="
+                                        device === 'lhr_desktop_result'
+                                            ? 'desktop_mac'
+                                            : 'phone_android'
+                                    "
+                                    :color="
+                                        $parent.calculateScoreType(
+                                            fullSpeedResultPage.meta[device]
+                                                .audits[key].score * 100
+                                        ).color
+                                    "
                                 />
                             </div>
                         </q-item-section>
@@ -83,16 +177,16 @@ export default {
     props: {
         active: {
             type: Boolean,
-            default: false,
+            default: false
         },
         fullSpeedResultPage: {
             type: Object,
-            default: () => ({}),
+            default: () => ({})
         },
         fullSpeedAuditsNames: {
             type: Array,
-            default: () => [],
-        },
-    },
+            default: () => []
+        }
+    }
 };
 </script>
